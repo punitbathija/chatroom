ffa768e2a7e1c409ae9668ed1713f75af8ab0dfc
Punit Bathija <punit644@gmail.com>
2023-01-09 14:05:22 +0530

    Building an home page for the application

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Signup from "./Components/Signup";
 import firebaseConfig from "./config/firebase.config";
 import { initializeApp } from "firebase/app";
 import Chatroom from "./Components/Chatroom";
+import Home from "./Components/Home";
 
 function App() {
   const firebase = initializeApp(firebaseConfig);
   return (
     <div className="App">
+      <Home />
       <Login />
       <Signup />
       <Chatroom />
