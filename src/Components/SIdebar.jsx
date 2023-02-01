62ebd0b2f5e58dc601173ba73c2b4f3be53938bb
Punit Bathija <punit644@gmail.com>
2023-02-01 13:21:39 +0530

    Error while creating userChats object in firebase

--- src/Components/SIdebar.jsx
+++ src/Components/SIdebar.jsx
@@ -8,6 +8,7 @@ import {
   getFirestore,
   collection,
   addDoc,
+  getDoc,
   query,
   orderBy,
   limit,
@@ -40,9 +41,25 @@ function Sidebar() {
         ? currentUser.uid + user.uid
         : user.uid + currentUser.uid;
     try {
-      const res = await getDocs(db, "chats", combinedId);
-      if (!res.exsist()) {
-        await setDoc(doc, (db, "chats", combinedId), { messages: [] });
+      const res = await getDoc(doc(db, "chats", combinedId));
+      if (!res.exists()) {
+        await setDoc(doc(db, "chats", combinedId), { messages: [] });
+        await updateDoc(doc(db, "userChats", currentUser.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
+        await updateDoc(doc(db, "userChats", user.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
       }
     } catch (err) {
       console.log(err);
--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -14,43 +14,55 @@ import {
 import { useDispatch } from "react-redux";
 import { login } from "../features/userSlice";
 import { getFirestore, collection, addDoc } from "firebase/firestore";
+import MonochromePhotosIcon from "@mui/icons-material/MonochromePhotos";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { v4 } from "uuid";
 
 function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [displayName, setDisplayName] = useState("");
   const [photoURL, setPhotoURL] = useState("");
-
+  const [file, setFile] = useState(null);
   const firebase = initializeApp(firebaseConfig);
   const auth = getAuth(firebase);
   const dispatch = useDispatch();
 
   async function signUp() {
-    await createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredentials) => {
-        updateProfile(userCredentials.user, {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+
+    // const date = new Date().getTime();
+    const profilePictureRef = ref(getStorage(), `${displayName + v4()}`);
+
+    await uploadBytesResumable(profilePictureRef, file).then(() => {
+      getDownloadURL(profilePictureRef).then(async (downloadUrl) => {
+        await updateProfile(res.user, {
           displayName: displayName,
-          photoURL: photoURL,
+          photoURL: downloadUrl,
         });
-        dispatch(
-          login({
-            email: userCredentials.user.email,
-            uid: userCredentials.user.uid,
-            displayName: userCredentials.user.displayName,
-            photoURL: userCredentials.user.photoURL,
-          })
-        );
-        console.log("User Signed Up" + userCredentials.user);
-        const userRef = addDoc(collection(getFirestore(), "users"), {
-          email: email,
-          uid: userCredentials.user.uid,
-          displayName: displayName,
-          photoURL: photoURL,
-        });
-      })
-      .catch((error) => {
-        console.log(error);
       });
+
+      dispatch(
+        login({
+          email: res.user.email,
+          uid: res.user.uid,
+          displayName: res.user.displayName,
+          photoURL: res.user.photoURL,
+        })
+      );
+      console.log("User Signed Up" + res.user);
+      const userRef = addDoc(collection(getFirestore(), "users"), {
+        email: email,
+        uid: res.user.uid,
+        displayName: displayName,
+        photoURL: photoURL,
+      });
+    });
   }
 
   async function signupWGoogle() {
@@ -73,6 +85,9 @@ function Signup() {
       });
     });
   }
+  const handleFile = (e) => {
+    setFile(e.target.files[0]);
+  };
 
   return (
     <div className="signin">
@@ -90,30 +105,33 @@ function Signup() {
         <TextField
           color="error"
           id="outlined-basic"
-          label="password"
+          label="Password"
           variant="outlined"
-          type="Password"
+          type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
         <TextField
           color="error"
           id="outlined-basic"
-          label="Display name"
+          label="Display Name"
           variant="outlined"
           type="text"
           value={displayName}
           onChange={(e) => setDisplayName(e.target.value)}
         />
-        <TextField
-          color="error"
-          id="outlined-basic"
-          label="Profile picture link"
-          variant="outlined"
-          type="text"
-          value={photoURL}
-          onChange={(e) => setPhotoURL(e.target.value)}
-        />
+        <div className="profilePicture">
+          <input
+            id="image"
+            type="file"
+            className="imageInput"
+            onChange={handleFile}
+          />
+          <label htmlFor="image" className="box">
+            <MonochromePhotosIcon />
+            Add a Profile Picture
+          </label>
+        </div>
         <Button color="error" variant="contained" fullWidth onClick={signUp}>
           Sign Up
         </Button>
